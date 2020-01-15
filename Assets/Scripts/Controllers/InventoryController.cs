e1ba39c477218525f709d2d6ddb10d6d014de7b0
s3717566 <s3717566@student.rmit.edu.au>
2020-01-16 00:07:41 +1100

    playing around

---
+++ Assets/Scripts/Controllers/InventoryController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryController : MonoBehaviour
+{
+    public static InventoryController instance;
+    public List<ItemStack> inventory; 
+
+    void Start() {
+        if (instance != null) {
+            Debug.log("InventoryController can only have one instance!");
+            return;
+        }
+        instance = this;
+    }
+
+    public void AddItemToInventory(ItemStack itemStack) {
+        bool hasItem = false;
+
+        foreach (ItemStack i in inventory) {
+            if (i.item.name == itemStack.item.name);
+            i.amount += itemStack.amount;
+            hasItem = true;
+        }
+        if (!hasItem) {
+            inventory.Add(itemStack);
+        }
+    }
+}
---
+++ Assets/Scripts/Controllers/ItemController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class ItemController : MonoBehaviour
+{
+     public Item[] itemList;
+
+     public Text itemDisplayText;
+
+     public void getItem() {
+         Item item = itemList[Random.Range(0, itemList.Length)].name;
+         InventoryController.instance.AddItemToInventory(new ItemStack(item, 1));
+         itemDisplayText.text = item.name;
+     }
+}
