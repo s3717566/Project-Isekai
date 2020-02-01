bddf5a16712b5b1146ae3b01fe023dc4bb8ef733
s3717566 <s3717566@student.rmit.edu.au>
2020-02-01 18:04:56 +1100

    added some uncommited files

---
+++ Assets/Scripts/Models/EquippableItem.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum EquipmentType {
+    Weapon,
+    Helmet,
+    Chestplate,
+    Leggings,
+    Boots,
+    Necklace,
+    Earring1,
+    Earring2,
+}
+
+[CreateAssetMenu]
+public class EquippableItem : Item
+{
+    public int PhysicalAttack;
+    public int PhysicalDefense;
+    public int MagicalAttack;
+    public int MagicalDefense;
+    [Space]
+    public int PhysicalAttackPercent;
+    public int PhysicalDefensePercent;
+    public int MagicalAttackPercent;
+    public int MagicalDefensePercent;
+
+    public EquipmentType EquipmentType;
+}
