4bd4a4d5a1200ca6e9976016841251fc2ba5b21a
GitAYYYer <duc.h@hotmail.com>
2020-02-06 00:55:03 +1100

    shows members, button will set unlock to true

--- Assets/Scripts/Models/Inventory/HaremStorage.cs
+++ Assets/Scripts/Models/Inventory/HaremStorage.cs
@@ -29,4 +29,16 @@ public class HaremStorage : ScriptableObject {
             //Debug.Log("in the harem storage: " + m.ToString());
         }
     }
+
+    //Return a list of all members who have unlocked = false.
+    public List<Member> getAllNotUnlocked() {
+        List<Member> returnList = new List<Member>();
+
+        foreach (Member member in members) {
+            if (!member.unlocked) {
+                returnList.Add(member);
+            }
+        }
+        return returnList;
+    }
 }
